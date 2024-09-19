b19ef22bf96106a17d1b63c514e5c1308b2b7e09
ElisEngstrom <elis-engstrom@hotmail.com>
2024-09-19 13:17:05 +0200

    I made the same program in C, very cool

---
+++ TestCode/helloC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void) {
+    printf("Hello world!\n");
+
+    int x = 0;
+    printf("write a number: ");
+    scanf("%d", &x);
+    printf("You entered: %d as your number.\n", x);
+
+    return 0;
+}
